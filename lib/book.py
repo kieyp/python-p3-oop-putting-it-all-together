f9fc76bf66b51e4453d9ac2c3970e02287683310
Boniface <cheruiyotbony@gmail.com>
2024-01-21 17:53:28 +0300

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,34 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
+
+    def __init__(self, title='The rising of Boniface cheruiyot', page_count=200):
+        self._title = title
+        self._page_count = page_count
     
-        
+
+    def get_title(self):
+        return self._title
+
+    def set_title(self, new_title):
+        self._title = new_title
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, page_count):
+        if type(page_count) is int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    title = property(get_title, set_title)
+    page_count = property(get_page_count, set_page_count)
+
+# Instantiate the Book class
+my_book = Book(title='Boniface cheruiyot')
+print(my_book.title)  # Output: Boniface cheruiyot
+
+my_book.turn_page()  # Output: Flipping the page...wow, you read fast!
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,72 @@
 #!/usr/bin/env python3
 
+# class Shoe:
+    
+#     def __init__(self, brand, size=9):
+#         self._brand = brand
+#         self._size = size
+#         self._condition = 'New'  # Initialize condition to 'Used'
+        
+#     def get_brand(self):
+#         return self._brand 
+    
+#     def set_brand(self, new_brand):
+#         self._brand = new_brand
+        
+#     def get_size(self):
+#         return self._size 
+    
+#     def set_size(self, new_size):
+#         if type(new_size) is  int:
+#             self._size = new_size
+#         else:
+#             print('Size must be an integer')
+            
+#     def get_condition(self):
+#         return self._condition
+        
+#     def set_condition(self, new_condition):
+#         if new_condition.lower() == 'new':
+#             print("The shoe has been repaired.")
+#             self._condition = 'New'
+#         else:
+#             print("Invalid condition. The condition remains as 'Used'.")
+            
+#     def cobble(self):
+#         print('Your shoe is as good as new!')
+#         self._condition = 'New'  
+        
+#     brand = property(get_brand, set_brand)  
+#     size = property(get_size, set_size)  
+#     condition = property(get_condition, set_condition)
+        
+        
+# luku = Shoe('Nike', 8)  
+# luku.brand = 'Adidas'  # Correcting brand assignment
+# luku.size = 'forty five' 
+# luku.condition = 'repaired'
+# print(luku.condition)
+
+# print(f"Boniface prefers {luku.brand} shoes, size: {luku.size}")
+
+#!/usr/bin/env python3
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else: 
+            print('size must be an integer')
+
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        Shoe.condition = "New"
+
+    size = property(get_size, set_size)
